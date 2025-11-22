4d98ca3f22d958267888dab951c0f8839426a227
Luis Sanchez <luisanchezdh@gmail.com>
2025-11-22 10:51:27 -0500

    fetched API data and set states for products, loading, and error

--- shopping-cart/pages/Layout.jsx
+++ shopping-cart/pages/Layout.jsx
@@ -1,6 +1,33 @@
 import { Outlet, Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const Layout = () => {
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const url = "https://fakestoreapi.com/products";
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error("Products not found!");
+        }
+        const data = await response.json();
+        console.log(data);
+        setProducts(data);
+      } catch (err) {
+        console.error("Error in API call", err);
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchProducts();
+  }, []);
+  console.log(products);
   return (
     <div>
       <nav>
