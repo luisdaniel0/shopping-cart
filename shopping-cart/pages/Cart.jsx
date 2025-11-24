3190f2b656a1e9061e41ab31eb1853a0b9809fd8
Luis Sanchez <luisanchezdh@gmail.com>
2025-11-24 10:01:58 -0500

    added cart total to cart page

--- shopping-cart/pages/Cart.jsx
+++ shopping-cart/pages/Cart.jsx
@@ -11,12 +11,18 @@ const Cart = () => {
     decreaseQuantity,
     removeFromCart,
   } = useOutletContext();
+
   if (loading) {
     return <h1>Loading..</h1>;
   }
   if (error) {
     return <h1>Error..{error}</h1>;
   }
+
+  const totalPrice = cart.reduce((total, cartItem) => {
+    const product = products.find((p) => p.id === cartItem.productId);
+    return total + Math.round(product.price) * cartItem.quantity;
+  }, 0);
   return (
     <div>
       <h1>Cart</h1>
@@ -30,6 +36,7 @@ const Cart = () => {
           removeFromCart={removeFromCart}
         />
       ))}
+      <h3>Your total: {totalPrice}</h3>
     </div>
   );
 };
